4cdb277af58e61885ca922d908fb72e71a07a724
Emil Rehnberg <emil.rehnberg@gmail.com>
2015-09-01 15:56:03 +0200

    adds abbr tag adjusting script w example
    i have abbr tags with and without id set in my anki collection
    CSS inserts abbreviation explanations on hover by matching on the id
    attribute, so the id attribute is preferably always set.

---
+++ _reassure-abbr-tag-id-and-adjust-innerHTML.js
@@ -0,0 +1,15 @@
+var abbrs = document.getElementsByTagName("abbr");
+for (var abbrNum in abbrs){
+  var abbr = abbrs[abbrNum];
+  abbr = adjustAbbr(abbr);
+}
+
+function adjustAbbr(abbr){
+  if (abbr.id) {
+    var letters = abbr.id;
+  } else {
+    var letters = abbr.innerHTML;
+    abbr.innerHTML = "»";
+  }
+  abbr.id = letters.toLowerCase();
+}
