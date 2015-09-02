e0bfddb7003de2c17f8abf5eee98d9f16bd5836f
Emil Rehnberg <emil.rehnberg@gmail.com>
2015-09-02 08:31:18 +0200

    adds insert word links script with examples
    this script is used to insert a-tags for words. So far used in the kanji
    templates for listing links to words using the kanji in question.
    
    links are seperated with a span containing a space.
    please not that the seperator is a closure. I was force to do this due
    to elements being stored by reference, hence new instances of the span
    needs to be inserted into the tags array.

---
+++ _insert-word-links.js
@@ -0,0 +1,23 @@
+var outId = "wordLinks";
+require(["_tag-builders", "_dom-readers", "_dom-writers", "_array-helpers"], function(tagBuilders, domReaders, domWriters, arrayHelpers){
+  var wordElementsIds = domReaders.readWords(["linkIds"]);
+  var words = domReaders.readWords(wordElementsIds);
+  if (words.length == 0) { return; }
+  var wordATags = mkWordATags(words);
+  var tags = arrayHelpers.insertSeparators(wordATags, tagBuilders.builder("span", " "));
+  domWriters.appendTags(outId, tags);
+
+  function mkWordATags(words){
+    var tags = [];
+    for (var wordNum in words) {
+      var word = words[wordNum];
+      tags = tags.concat(tagBuilders.buildAnchor(word, wordDictUrl(word)));
+    };
+    return tags;
+  }
+
+  function wordDictUrl(word){
+    return ["http://dictionary.goo.ne.jp/srch/all", word, "m0u/"].join("/");
+  }
+
+});
