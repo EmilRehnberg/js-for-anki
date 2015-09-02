87a42ff9b84f772004044dd282f8316028a9ab78
Emil Rehnberg <emil.rehnberg@gmail.com>
2015-09-02 11:01:00 +0200

    adds script for inserting data structures table
    table is for data structures in different languages

---
+++ _insert-data-structures-table.js
@@ -0,0 +1,32 @@
+var adjacentCodeExamples = {
+  Ruby: ["[1, 2]", "{a: 1}", "[1, 2].freeze"],
+  JavaScript: ["[1, 2]", "{a: 1}", ""],
+  R: ["c(1, 2)", "list(a=1)", ""],
+  Python: ["[1, 2]", "{'a': 1}", "(1, 2)"],
+  CSS: ["(1, 2)", "(a: 1)", ""],
+  SQL: ["(1, 2)", "", ""],
+};
+
+var data = {
+  header: ["list", "mapping", "imm list"],
+  tableClass: "small",
+  tableData: {
+    Ruby: ["Array", "Hash", ""],
+    JavaScript: ["array", "map", ""],
+    R: ["vector", "list", ""],
+    Python: ["list", "dict", "tuple"],
+    CSS: ["list", "map", ""],
+    SQL: ["", "", ""],
+  }
+};
+
+for (language in adjacentCodeExamples) {
+  for (codeExampleNum in adjacentCodeExamples[language]) {
+    var example = adjacentCodeExamples[language][codeExampleNum];
+    var exampleTagString = ["<code>", example, "</code>"].join("");
+    var structName = data["tableData"][language][codeExampleNum];
+    data["tableData"][language][codeExampleNum] = [structName, exampleTagString].filter(Boolean).join(": ");
+  }
+};
+
+require(["_tables"], function(tables){ tables.insertToDoc(data); });
