57976068052b13453bd667a7c0d23188e06d1923
Emil Rehnberg <emil.rehnberg@gmail.com>
2015-09-04 07:37:21 +0200

    changes insert-word-links to insert a details tag
    instead of the templates having the details markup, I let the javascript
    append a details tag with the links

--- _insert-word-links.js
+++ _insert-word-links.js
@@ -1,17 +1,23 @@
 var outId = "word-links-writer";
-require(["_tag-builders", "_dom-readers", "_dom-writers", "_array-helpers"], function(tagBuilders, domReaders, domWriters, arrayHelpers){
+var articleId = "main";
+require(["_tag-builders", "_dom-readers", "_dom-writers", "_array-helpers"], function(builders, domReaders, domWriters, arrayHelpers){
   var wordElementsIds = domReaders.readWords(["link-reader-ids"]);
   var words = domReaders.readWords(wordElementsIds);
   if (words.length == 0) { return; }
   var wordATags = mkWordATags(words);
-  var tags = arrayHelpers.insertSeparators(wordATags, tagBuilders.builder("span", " "));
+
+  var detailsElement = builders.buildDetailsTag("", "単語リンク");
+  detailsElement.id = outId;
+  domWriters.appendTags(articleId, [detailsElement]);
+
+  var tags = arrayHelpers.insertSeparators(wordATags, builders.builder("span", " "));
   domWriters.appendTags(outId, tags);
 
   function mkWordATags(words){
     var tags = [];
     for (var wordNum in words) {
       var word = words[wordNum];
-      tags = tags.concat(tagBuilders.buildAnchor(word, wordDictUrl(word)));
+      tags = tags.concat(builders.buildAnchor(word, wordDictUrl(word)));
     };
     return tags;
   }
