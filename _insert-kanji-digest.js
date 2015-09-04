29240ccf8b9ada50731b7a527f42d330be8dda5e
Emil Rehnberg <emil.rehnberg@gmail.com>
2015-09-04 07:39:35 +0200

    alters insert-kanji-digest to insert a details tag
    instead of looking for markup to insert the kanji-digest, the script now
    appends a details tag with the digest

--- _insert-kanji-digest.js
+++ _insert-kanji-digest.js
@@ -1,22 +1,22 @@
-var readerId = "text-reader";
-var writerId = "kanji-digest-writer";
+require(["_dom-readers", "_dom-writers", "_tag-builders"], function(readers, writers, builders){
+  var readerId = "text-reader";
+  var articleId = "main";
 
-var kanji = readKanji(readerId);
-writeKanji(writerId ,kanji);
+  var kanji = readKanji(readerId);
+  writeKanji(articleId, kanji);
 
-function readKanji(readerId){
-  var readerElement = document.getElementById(readerId);
-  if (readerElement == undefined) {return ;}
-  return extractKanji(readerElement.innerHTML);
-}
+  function readKanji(readerId){
+    var text = readers.readTagContents(readerId);
+    return extractKanji(text);
+  }
 
-function writeKanji(writerId, kanji){
-  if (Boolean(kanji) == false) { return; }
-  var writerElement = document.getElementById(writerId);
-  if (writerElement == undefined){ return; }
-  writerElement.insertAdjacentText('beforeEnd', kanji);
-}
+  function writeKanji(articleId, kanji){
+    if (Boolean(kanji) == false) { return; }
+    var detailsElement = builders.buildDetailsTag(kanji, "単語を入る補");
+    writers.appendTags(articleId, [detailsElement]);
+  }
 
-function extractKanji(txt) {
-  return txt.match(/[\u4E00-\u9FAF]/g).join("");
-}
+  function extractKanji(txt) {
+    return txt.match(/[\u4E00-\u9FAF]/g).join("");
+  }
+});
