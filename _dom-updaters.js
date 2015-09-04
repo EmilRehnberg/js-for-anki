e773d80726d17b5e9cb0b44da6a8a92784ffb756
Emil Rehnberg <emil.rehnberg@gmail.com>
2015-09-04 17:14:51 +0200

    adds dom updater module
    used so far only for manipulating cloze templates

---
+++ _dom-updaters.js
@@ -0,0 +1,54 @@
+define(["_dom-readers", "_dom-writers", "_tag-builders"], function(readers, writers, builders){
+  var domUpdaterFunctions = {
+    insertHint: insertHint,
+    replaceClozeExpr: replaceClozeExpr,
+    replaceWithLength: replaceWithLength,
+  };
+
+  return domUpdaterFunctions;
+
+  function replaceClozeExpr(expression, fullTextId){
+    var firstClozedSpan = readers.readClozeSpans()[0];
+    firstClozedSpan.outerHTML = "";
+    var textElement = readers.readElement(fullTextId);
+    exprRegex = new RegExp(expression, "g");
+    textElement.innerHTML = textElement.innerHTML.replace(exprRegex, firstClozedSpan.outerHTML);
+  }
+
+  function insertHint(expression, fullTextId){
+    replaceClozedWLength(expression);
+    insertClozedTextHint(expression, fullTextId);
+  }
+
+  function insertClozedTextHint(expr, fullTextId){
+    var hintId = "hint";
+    var pHintTag = builders.buildWriterP(hintId);
+
+    writers.appendTags(fullTextId, [pHintTag], "afterEnd");
+    var hintElement = builders.buildDefDfn(expr);
+    writers.appendTags(hintId, [hintElement]);
+  }
+
+  function replaceClozedWLength(expr) {
+    var clozeSpans = readers.readClozeSpans();
+    for (clozeNum in clozeSpans){
+      replaceWithLength(clozeSpans[clozeNum], expr);
+    }
+  }
+
+  function replaceWithLength(element, expression){
+    element.innerHTML = dottifyExpression(expression);
+  }
+
+  function dottifyExpression(expression){
+    return dottifyTxt(removeNumbers(expression));
+  }
+
+  function removeNumbers(expression){
+    return expression.replace(/[\d]/g, "");
+  }
+
+  function dottifyTxt(expression){
+    return expression.replace(/./g, "・");
+  }
+});
