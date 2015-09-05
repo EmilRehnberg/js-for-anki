3319baf9902c8b736961babab760c65bd44f1d89
Emil Rehnberg <emil.rehnberg@gmail.com>
2015-09-05 20:40:13 +0200

    adds script for inserting data types tables
    adds two examples
    - one vanilla example
    - one using both the data tables as it was buggy
      (scoping in require lead to same table being inserted twice)

---
+++ _insert-data-types-table.js
@@ -0,0 +1,18 @@
+require(["_tables"], function(tables){
+  var data = {
+    header: ["1", "1.0", "a", "true"],
+    tableClass: "small",
+    caption:"Data Types",
+    tableData: {
+      Ruby: ["Integer", "Float", "String", "TrueClass"],
+      JavaScript: ["number", "number", "string", "boolean"],
+      R: ["integer", "double", "character", "logical"],
+      Python: ["int", "float", "str", "true"],
+      Go: ["int", "float64", "string", "bool"],
+      CSS: ["integer", "number", "string", "boolean"],
+      SQL: ["INTEGER(p)", "FLOAT(p)", "CHARACTER", "BOOLEAN"],
+  }
+};
+
+tables.writeTable(data);
+});
