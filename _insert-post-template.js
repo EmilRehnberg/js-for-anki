31301e144da046f16113810613828f19a89a6e23
Emil Rehnberg <emil.rehnberg@gmail.com>
2015-09-07 10:41:11 +0200

    adds modules for inserting templates pre and post pre and post main lines

---
+++ _insert-post-template.js
@@ -0,0 +1,3 @@
+require(["_dom-readers", "_dom-writers"],function(readers, writers){
+  writers.writeTemplates("post-templates-reader", "post-templates-writer");
+});
---
+++ _insert-pre-template.js
@@ -0,0 +1,3 @@
+require(["_dom-readers", "_dom-writers"],function(readers, writers){
+  writers.writeTemplates("pre-templates-reader", "pre-templates-writer");
+});
