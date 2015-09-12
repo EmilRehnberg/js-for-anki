35486152a79170fbb6caacf023aa239dba18941b
Emil Rehnberg <emil.rehnberg@gmail.com>
2015-09-12 12:33:47 +0200

    removes unnessecary entries in dfn-tags script
    previously the insert-dfn-tags script built dfn-tags for both kanji and
    other templates.
    
    the kanji template dfn-tag rendering is performed by kanji-template
    specific script nowdata.

--- _insert-dfn-tags.js
+++ _insert-dfn-tags.js
@@ -1,39 +1,5 @@
 require(["_dom-writers", "_tag-builders"], function(writers, builders){
   var wordSets = {
-    wago: {
-      readerId: "wago-reader",
-      writerId: "wago-writer",
-      builder: builders.buildJaDfnTag,
-      seperator: builders.buildBr
-    },
-    kango: {
-      readerId: "kango-reader",
-      writerId: "kango-writer",
-      builder: builders.buildJaDfnTag,
-      seperator: builders.buildBr
-    },
-    namae: {
-      readerId: "namae-reader",
-      writerId: "namae-writer",
-      builder: builders.buildNameDfnTag
-    },
-    eigoWago: {
-      readerId: "wago-reader",
-      writerId: "eigo-writer",
-      builder: builders.buildEnDfnTag,
-      seperator: builders.buildBr
-    },
-    eigoKango: {
-      readerId: "kango-reader",
-      writerId: "eigo-writer",
-      builder: builders.buildEnDfnTag,
-      seperator: builders.buildBr
-    },
-    chigai: {
-      readerId: "chigai-reader",
-      writerId: "chigai-writer",
-      builder: builders.buildReadingDfnTag
-    },
     imi: {
       readerId: "imi-reader",
       writerId: "definitions-writer",
@@ -45,7 +11,7 @@ require(["_dom-writers", "_tag-builders"], function(writers, builders){
       writerId: "definitions-writer",
       builder: builders.buildReadingDfnTag
     },
-    namae2: {
+    namae: {
       readerId: "namae-reader",
       writerId: "definitions-writer",
       builder: builders.buildNameDfnTag
