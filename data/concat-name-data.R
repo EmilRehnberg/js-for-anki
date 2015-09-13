b34c57b9576db6f6697b034775d0e5fa0384cec1
Emil Rehnberg <emil.rehnberg@gmail.com>
2015-09-13 09:22:39 +0200

    moves compilation of entity data to an R setup
    I thinks it's nicer to add and maintain csv data.
    R is pretty good at data manipulation so I decided to do reading and
    writing of the data via an R script.
    
    The script is like before executed by ./copy_to_anki

---
+++ data/concat-name-data.R
@@ -0,0 +1,96 @@
+#!/usr/bin/env Rscript
+
+library(tools)
+library(magrittr)
+outFile = "_entity-data.js"
+nameDataDir = "data"
+metaFileName = "names-data-meta.csv"
+metaPath = file.path(nameDataDir, metaFileName)
+
+readMeta <- function() {
+  read.csv(metaPath, stringsAsFactors=FALSE, row.names=1)
+}
+
+listNameDataFiles <- function(){
+  list.files(nameDataDir, pattern="*csv") %>%
+    setdiff(metaFileName)
+}
+
+getCsvFilePaths <- function(){
+  listNameDataFiles() %>%
+    file.path(nameDataDir, .)
+}
+
+readNameCSV <- function(path){
+  read.csv(path,
+           col.names=c("name", "reading", "description"),
+           header=FALSE,
+           stringsAsFactors=FALSE
+           )
+}
+
+setNameFileNames <- function(nameFileList){
+  names(nameFileList) <- file_path_sans_ext(listNameDataFiles())
+  nameFileList
+}
+
+buildNamesData <- function(){
+  getCsvFilePaths() %>%
+    lapply(., readNameCSV) %>%
+    setNameFileNames
+}
+
+insertMetaData <- function(dataList, meta){
+  for(dataName in names(dataList)){
+    for(attrName in colnames(meta)){
+      dataList[[dataName]][attrName] <- meta[dataName, attrName]
+    }
+  }
+  dataList
+}
+
+buildNameDataRow <- function(row){
+  sprintf('%s: ["%s","%s","%s","%s"],', row[1], row[2], row[3], row[4], row[5])
+}
+
+buildNameDataRows <- function(nameSetsList){
+  lapply(nameSetsList, function(nameSet){
+    apply(nameSet, 1, buildNameDataRow)
+  }) %>% unlist
+}
+
+writeNameDataFile <- function(nameSetsList, outFilePath){
+  c("define(function(){ return {",
+    buildNameDataRows(nameSetsList),
+    "}; });"
+    ) %>%
+    paste(., collapse="\n") %>%
+    write(file=outFilePath)
+}
+
+mTimeSecs <- function(path){
+  file.info(path)$mtime %>% as.numeric
+}
+
+dataIsUpdated <- function(){
+  if(!file.exists(outFile)){ return(TRUE) }
+  outFileMTime <- mTimeSecs(outFile)
+  nameDataMTimes <- lapply(getCsvFilePaths(), mTimeSecs) %>% unlist
+  c(outFileMTime, nameDataMTimes) %>% max != outFileMTime
+}
+
+run <- function(){
+  if(dataIsUpdated()){
+    message("  names data is updated, new js-version created")
+  }else{
+    message("  no name updates; nothing new compiled")
+    return()
+  }
+  meta = readMeta()
+  buildNamesData() %>%
+    insertMetaData(., meta) %>%
+    writeNameDataFile(., outFile)
+  message("  compiled new name data")
+}
+run()
+
--- lib/entity_data_compiler.rb
+++ lib/entity_data_compiler.rb
@@ -1,33 +1,10 @@
 require 'pathname'
 
 class EntityDataCompiler
-  BASE_SCRIPT = "names-merger.js"
-  DATA_DIR = Pathname.new("data")
-  OUTPUT_FILE_NAME = "_entity-data.js"
-  JS_FILE_REGEX = Regexp.new(/[\w-]+\.js/)
+  BUILD_NAME_MODULE_SCRIPT_PATH = "data/concat-name-data.R"
 
   def self.run
-    # return if nothing_new?
-    remove_destination
-    File.open(OUTPUT_FILE_NAME, 'a'){ |handle|
-      name_sets.each { |set|
-        handle.puts File.read(DATA_DIR + set)
-      }
-      handle.puts File.read(DATA_DIR + BASE_SCRIPT)
-    }
-  end
-
-  def self.name_sets
-    js_file_names - Array(BASE_SCRIPT)
-  end
-
-  def self.js_file_names
-    Dir.entries(DATA_DIR).select{ |file_name|
-      file_name[JS_FILE_REGEX]
-    }
-  end
-
-  def self.remove_destination
-    File.delete(OUTPUT_FILE_NAME) if File.exist?(OUTPUT_FILE_NAME)
+    puts "compiling name data"
+    %x(./#{BUILD_NAME_MODULE_SCRIPT_PATH})
   end
 end
