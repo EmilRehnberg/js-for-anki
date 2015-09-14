bdcb79e89ae8333caa8eb91e13cac1cb99298751
Emil Rehnberg <emil.rehnberg@gmail.com>
2015-09-14 11:05:32 +0200

    adds template for `airquality` data-set
    shows the first line in the R dataset `airquality`

---
+++ _tmpl-r-airquality-sample.js
@@ -0,0 +1,18 @@
+define(["_tag-builders"], function(builders){
+  var io = {
+    i: [
+      "airquality",
+    ],
+    o: [
+      "   Ozone Solar.R Wind Temp Month Day",
+      "1     41     190  7.4   67     5   1",
+      "2     36     118  8.0   72     5   2",
+      "3     12     149 12.6   74     5   3",
+      "4     18     313 11.5   62     5   4",
+      "5     NA      NA 14.3   56     5   5",
+      "...",
+    ]
+  };
+
+  return builders.buildCodePair(io);
+});
