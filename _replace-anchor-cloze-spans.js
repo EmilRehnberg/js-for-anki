d4ccdb163bf8dc5ff655424901798fd12d9d00d0
Emil Rehnberg <emil.rehnberg@gmail.com>
2015-09-30 04:19:05 +0200

    removes replace anchor cloze spans

--- _replace-anchor-cloze-spans.js
+++
@@ -1,8 +0,0 @@
-require(["_dom-updaters", "_dom-readers"], function(updaters, readers){
-  var readerId = "text-reader";
-
-  var anchorHint = readers.readAnchorContent();
-  if (anchorHint){
-    updaters.insertHint(anchorHint, readerId);
-  }
-});
