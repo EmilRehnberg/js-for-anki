6fd081e51d8ab357f84485cc8df89618325d05dc
Emil Rehnberg <emil.rehnberg@gmail.com>
2019-07-04 21:29:07 +0200

    update adjacent execution with standardjs

--- _adjacent-execution.js
+++ _adjacent-execution.js
@@ -1,22 +1,22 @@
-require(["_dom-readers", "_dom-writers", "_tag-builders"], function(readers, writers, builders){
-  var execScripts = readers.readWords(["execute-scripts"]);
-  execScripts.forEach(printScriptTag);
+require(['_dom-readers', '_dom-writers', '_tag-builders'], function (readers, writers, builders) {
+  var execScripts = readers.readWords(['execute-scripts'])
+  execScripts.forEach(printScriptTag)
 
-  function buildScript(scriptName){
-    var prependPath = readPrependedPath();
-    var code = ['require(["', prependPath, '_', scriptName, '.js"]);'].join("");
-    return builders.buildScript(code);
+  function buildScript (scriptName) {
+    var prependPath = readPrependedPath()
+    var code = ['require(["', prependPath, '_', scriptName, '.js"]);'].join('')
+    return builders.buildScript(code)
   }
 
-  function printScriptTag(scriptName){
-    writers.appendToBody(buildScript(scriptName));
+  function printScriptTag (scriptName) {
+    writers.appendToBody(buildScript(scriptName))
   }
 
   // only used in examples
-  function readPrependedPath(){
-    var prependId = "prepend-path";
-    var pathElement = document.getElementById(prependId);
-    if(pathElement == undefined){ return ""; };
-    return pathElement.innerHTML;
+  function readPrependedPath () {
+    var prependId = 'prepend-path'
+    var pathElement = document.getElementById(prependId)
+    if (pathElement === undefined) { return '' };
+    return pathElement.innerHTML
   }
-});
+})
